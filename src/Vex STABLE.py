d42542fb748eae1ae36185505f19801a442c915c
IKnowWhyImHere <matteo-pepe@outlook.com>
2026-02-24 14:47:25 -0500

    Update to VEX Stable
    Reminder, Vex Stable is supposed to serve as a stable, working version of the code that works on the current model of the robot. All commented out functions should be removed in this code

--- src/Vex STABLE.py
+++ src/Vex STABLE.py
@@ -1,14 +1,25 @@
 # vex 1155B Kestrel
 
+
+
+
+#THIS IS STABLE, DO NOT UPDATE UNTIL TEST WORKS BETTER ON CURRENT ROBOT THAN THIS
+"""Reminder, Vex Stable is supposed to serve as a stable, working version of the code that
+works on the current model of the robot. All commented out functions should be removed in this code"""
+
+
+
 import time
 from vex import *
 import math
 
 """Global and Necessary Definitions"""
 
-plusminus = 1
+plusminus = 2
+hoppercycle = 2
 
 brain=Brain() #defines the brain variable, IMPORTANT: DO NOT REMOVE
+#SLOT NUMBER 17 DOES NOT WORK
 
 ControllerType.PRIMARY #what is the difference between this and the next line, are both necesary? 
 controller = Controller(PRIMARY)
@@ -45,16 +56,117 @@ drivetrain = SmartDrive(right_drive, left_drive, imu1,(3.25 * math.pi),13.95,13,
 #what about the smartdrivetrain class?
 """Other Motor Definitions"""
 
-intake = Motor(Ports.PORT17,GearSetting.RATIO_18_1, False) 
-intake.set_velocity(100, PERCENT)
+#According to Ryan on 2/19/2026 Port 17 is broken. Please verify.
 
 Tchain = Motor(Ports.PORT19,GearSetting.RATIO_18_1, False)
 Tchain.set_velocity(100, PERCENT)
 
-Ramp = Motor(Ports.PORT13,GearSetting.RATIO_18_1, False)
-Ramp.set_velocity(7,PERCENT)
+
+Intake = Motor(Ports.PORT8,GearSetting.RATIO_18_1, False)
+Intake.set_velocity(100,PERCENT)
 
 topspin = MotorGroup(outtakeL, outtakeR)
+
+DeScore = Motor(Ports.PORT9,GearSetting.RATIO_18_1, False)
+DeScore.set_velocity(100,PERCENT)
+
+HopperRamp = Motor(Ports.PORT12,GearSetting.RATIO_18_1)
+HopperRamp.set_velocity(100,PERCENT)
+
+RampPiston = Pneumatics(brain.three_wire_port.f)
+
+def TFor() :
+    if Tchain.direction()!=FORWARD:  
+        Tchain.spin(FORWARD)
+    else:  
+     Tchain.stop()
+
+def TRev():
+    if Tchain.direction()!=REVERSE:
+     Tchain.spin(REVERSE)
+    else:
+     Tchain.stop()
+
+def InFor():
+    if Intake.direction()!=FORWARD:
+        Intake.spin(FORWARD)
+    else:
+     Intake.stop()
+
+def InRev():
+    if Intake.direction()!=REVERSE:
+        Intake.spin(REVERSE)
+    else:
+     Intake.stop()
+
+def MoveDeScore():
+    global plusminus
+    if plusminus ==1:
+        DeScore.spin(FORWARD)
+    else:
+        DeScore.spin(REVERSE)
+
+def StopDeScore():
+    global plusminus
+    DeScore.stop()
+    if plusminus ==1:
+        plusminus += 1
+    else:
+        plusminus -= 1
+
+def MoveHopperRamp():
+   global hoppercycle
+   if hoppercycle ==1:
+        HopperRamp.spin(FORWARD)
+   else:
+        HopperRamp.spin(REVERSE)
+
+def StopHopperRamp():
+    global hoppercycle
+    HopperRamp.stop()
+    if hoppercycle ==1:
+        hoppercycle += 1
+    else:
+        hoppercycle -= 1
+
+def RampUp():
+    RampPiston.open()
+
+def RampDown():
+    RampPiston.close()
+
+#AI definitions - I object but at this point in time I shall humor ryan, I will no longer touch the code. When he can explain it to me then we shall talk.
+
+WHEEL_DIAMETER_IN = 4.0
+WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER_IN * math.pi
+TRACK_WIDTH_IN = 12.0
+
+DRIVE_SPEED = 50
+TURN_SPEED = 40
+OUTTAKE_SPEED = 100
+
+
+def drive_inches(distance):
+    degrees = (distance / WHEEL_CIRCUMFERENCE) * 360
+
+    LeftDF.spin_for(FORWARD, degrees, DEGREES, DRIVE_SPEED, PERCENT, False)
+    LeftDB.spin_for(FORWARD, degrees, DEGREES, DRIVE_SPEED, PERCENT, False)
+
+    RightDF.spin_for(FORWARD, degrees, DEGREES, DRIVE_SPEED, PERCENT, False)
+    RightDB.spin_for(FORWARD, degrees, DEGREES, DRIVE_SPEED, PERCENT, True)
+
+def turn_left(deg):
+    turn_circ = math.pi * TRACK_WIDTH_IN
+    wheel_dist = (deg / 360) * turn_circ
+    motor_deg = (wheel_dist / WHEEL_CIRCUMFERENCE) * 360
+
+    RightDF.spin_for(REVERSE, motor_deg, DEGREES, TURN_SPEED, PERCENT, False)
+    RightDB.spin_for(REVERSE, motor_deg, DEGREES, TURN_SPEED, PERCENT, False)
+
+    LeftDF.spin_for(FORWARD, motor_deg, DEGREES, TURN_SPEED, PERCENT, False)
+    LeftDB.spin_for(FORWARD, motor_deg, DEGREES, TURN_SPEED, PERCENT, True)
+
+
 
 #---------------------------------------------------------------#
 
@@ -66,31 +178,32 @@ def driver(): # sets up the driver controls, namely pressing what buttons on the
     global mode 
     is_driver=True
 
+    controller.buttonX.pressed(TFor)
+    controller.buttonB.pressed(TRev)
+
+    controller.buttonUp.pressed(InFor)
+    controller.buttonDown.pressed(InRev)
+
+    controller.buttonRight.pressed(MoveDeScore)
+    controller.buttonRight.released(StopDeScore)
+
+    controller.buttonL1.pressed(RampPiston.open)
+    controller.buttonR1.pressed(RampPiston.close)
+
+    controller.buttonLeft.pressed(MoveHopperRamp)
+    controller.buttonLeft.released(StopHopperRamp)
+
+
 
     while True:
         right_drive.set_velocity(controller.axis2.position(), PERCENT) #makes the right drive move by what percentage forward the stick is
         right_drive.spin(FORWARD)
 
         left_drive.set_velocity(controller.axis3.position(), PERCENT) #makes the left drive move by what percentage forward the stick is
-        left_drive.spin(FORWARD)
-
-        if controller.buttonX.pressing():
-            Tchain.spin(REVERSE)
-
-        if controller.buttonB.pressing():
-            Tchain.spin(FORWARD)
-          
-        if controller.buttonY.pressing():
-            Tchain.stop()
+        left_drive.spin(FORWARD)       
         
-        if controller.buttonUp.pressing():
-           intake.spin(FORWARD)
-
-        if controller.buttonDown.pressing():
-            intake.spin(REVERSE)
-        
-        if controller.buttonRight.pressing():
-            intake.stop()
+       # if controller.buttonRight.pressing():
+        #    intake.stop()
 
         if controller.buttonR2.pressing():
             topspin.spin(FORWARD)
@@ -100,14 +213,6 @@ def driver(): # sets up the driver controls, namely pressing what buttons on the
         
         if controller.buttonA.pressing():
             topspin.stop()
-        
-        if controller.buttonL1.pressing():
-            Ramp.spin(FORWARD)
-        elif controller.buttonR1.pressing():
-            Ramp.spin(REVERSE)
-        else:
-            Ramp.stop()
-        
         
         
         
@@ -119,6 +224,21 @@ def autonomous():
     global r_pos
     global l_pos
     global is_driver
-    is_driver = False
+    is_driver = False  
+    
+    RampPiston.close()
+    #Here begins AI code, once again I object, I shall not touch the code until Ryan can explain to me what it is that this does and the cost behind his making it.
+    # 1. Drive backwards 36 inches
+    drive_inches(-36)
+
+    # 2. Turn left 45 degrees
+    turn_left(47)
+
+    # 3. Drive backwards 8 inches
+    drive_inches(-8.5)
+
+    # 4. START OUTTAKE (BACKWARDS) AFTER MOVEMENT
+    outtakeL.spin(REVERSE, OUTTAKE_SPEED, PERCENT)
+    outtakeR.spin(REVERSE, OUTTAKE_SPEED, PERCENT)
 
 competition = Competition(driver,autonomous)
